c66b4a4494007038338947754b78993e22af3669
y-chiba1008 <chiba8e3@gmail.com>
2026-02-05 14:54:15 +0900

    feat: 全体のレイアウト

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,45 @@
-import { AbsoluteCenter, Text, VStack } from "@chakra-ui/react"
+import {
+    AbsoluteCenter, Heading, VStack,
+    createSystem, defineConfig, defaultConfig, ChakraProvider,
+} from "@chakra-ui/react"
 import AudioButton from "./components/AudioButton"
-import { useTranscriptStore } from "./hooks/useTranscriptStore";
+import SentenceArea from "./components/SentenceArea"
+
+// 背景色の設定
+const config = defineConfig({
+    globalCss: {
+        body: {
+            bg: "gray.50",
+        },
+    },
+});
+const system = createSystem(defaultConfig, config);
 
 function App() {
-    const sentence = useTranscriptStore((state) => state.sentence);
-    const errorMessage = useTranscriptStore((state) => state.errorMessage);
-
     return (
-        <AbsoluteCenter>
-            <VStack>
-                <AudioButton></AudioButton>
+        <ChakraProvider value={system}>
+            {/* ヘッダ */}
+            <Heading
+                textAlign="center" padding={3}
+                textDecoration="underline"
+                textDecorationColor="cyan.fg"
+                textDecorationThickness="2px"
+                textUnderlineOffset="3px"
+            >会話サポートアプリ</Heading>
 
-                {/* デバッグ用 */}
-                <Text>{sentence}</Text>
-                <Text>{errorMessage}</Text>
-            </VStack>
-        </AbsoluteCenter>
+            {/* コンテンツ */}
+            <AbsoluteCenter height="80vh">
+                <VStack width="500px" height="100%">
+                    <AudioButton width="100%" />
+                    <SentenceArea
+                        width="100%" height="100%"
+                        backgroundColor="bg.panel"
+                        borderWidth="1px"
+                        borderColor="border"
+                    />
+                </VStack>
+            </AbsoluteCenter>
+        </ChakraProvider>
     )
 }
 
