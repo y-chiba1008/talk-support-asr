10b24f2ef5b2a8f318aac5213094d9bd72acc025
y-chiba1008 <chiba8e3@gmail.com>
2026-02-07 10:19:03 +0900

    feat(backend): 文字起こしAPI 音声データ受け取り

--- backend/app/routers/transcript.py
+++ backend/app/routers/transcript.py
@@ -1,11 +1,39 @@
-from fastapi import APIRouter
+import io
+import logging
+
+import librosa
+from fastapi import APIRouter, File, HTTPException, UploadFile
 
 router = APIRouter()
+logger = logging.getLogger('uvicorn')
 
 @router.post('/')
-async def transcript():
-    """文字起こしAPI
-    """
+async def transcript(file: UploadFile = File(...)):
+    '''文字起こしAPI
+    '''
+    # バリデーション
+    if file.content_type not in ['audio/wav']:
+        raise HTTPException(status_code=400, detail='対応していないファイル形式です。')
+    
+    try:
+        # ファイルをメモリ上に読み込む
+        content = await file.read()
+        file_like_obj = io.BytesIO(content)
+
+        # librosaで読み込み(同時にサンプリングレートを16000に変換)
+        y, sr = librosa.load(file_like_obj, sr=16000)
+
+        # 長さ（秒）を計算
+        duration = librosa.get_duration(y=y, sr=sr)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'解析に失敗しました: {str(e)}')
+    finally:
+        await file.close()
+    
+    logger.info(f'duration: {duration}')
+    logger.info(f'sr: {sr}')
+
     response = {
         'sentence': 'ダミー from FastAPI',
     }
