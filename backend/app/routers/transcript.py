f0efdaf39c33e12e0cb556768bf2d8b33c816c0b
y-chiba1008 <chiba8e3@gmail.com>
2026-02-08 11:31:13 +0900

    feat(backend): ブラウザから受けとったデータを一時保存（確認用）
    ブラウザから受け取った音声データはlibrosaで読み込めなかったので、長さのログ出力は一旦削除する

--- backend/app/routers/transcript.py
+++ backend/app/routers/transcript.py
@@ -1,38 +1,32 @@
-import io
 import logging
+from pathlib import Path
+import shutil
 
-import librosa
 from fastapi import APIRouter, File, HTTPException, UploadFile
 
 router = APIRouter()
 logger = logging.getLogger('uvicorn')
 
-@router.post('/')
-async def transcript(file: UploadFile = File(...)):
+UPLOAD_DIR = Path('uploads')
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+@router.post('')
+async def transcript(audio: UploadFile = File(...)):
+    logger.info('transcript start')
     '''文字起こしAPI
     '''
     # バリデーション
-    if file.content_type not in ['audio/wav']:
+    if audio.content_type not in ['audio/wav']:
         raise HTTPException(status_code=400, detail='対応していないファイル形式です。')
     
+    file_path = UPLOAD_DIR / 'test.wav'
+
     try:
-        # ファイルをメモリ上に読み込む
-        content = await file.read()
-        file_like_obj = io.BytesIO(content)
-
-        # librosaで読み込み(同時にサンプリングレートを16000に変換)
-        y, sr = librosa.load(file_like_obj, sr=16000)
-
-        # 長さ（秒）を計算
-        duration = librosa.get_duration(y=y, sr=sr)
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f'解析に失敗しました: {str(e)}')
+        with file_path.open('wb') as buffer:
+            shutil.copyfileobj(audio.file, buffer)
     finally:
-        await file.close()
-    
-    logger.info(f'duration: {duration}')
-    logger.info(f'sr: {sr}')
+        # 最後にファイルを閉じる
+        await audio.close()
 
     response = {
         'sentence': 'ダミー from FastAPI',
