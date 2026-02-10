48be29fd5acac8668e92056e1fe95a1901bb73ff
y-chiba1008 <chiba8e3@gmail.com>
2026-02-10 12:47:48 +0900

    feat(backend): 開始終了ログミドルウェアの廃止
    Bad Requestの時など、ミドルウェアからログが出るのにルータまでは処理が進んでいない事があり、デバッグ用途として期待した動作と異なっていた

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import ping
 from app.routers import transcript
-from app.middlewares.logStartEndMiddleware import LogStartEndMiddleware
 from app.config import CONFIG
 
 app = FastAPI()
@@ -16,8 +15,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-## 開始/終了ログ
-app.add_middleware(LogStartEndMiddleware)
 
 # ルータを追加
 app.include_router(ping.router, prefix="/ping", tags=["ping"])
